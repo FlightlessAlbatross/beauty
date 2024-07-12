e7d42b32e55876173e407b0ba517844e30f30b31
FlightlessAlbatross <hofer1991@gmail.com>
2024-07-12 15:43:54 +0200

    split the OSM subsets into separte scripts and make them executable

---
+++ scripts/osm_extract_powerlines.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Function to display usage information
+usage() {
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
+}
+
+# Check for correct number of arguments
+if [ "$#" -ne 2 ]; then
+    usage
+fi
+
+# Assign command line arguments to variables
+input_file="$1"
+output_file="$2"
+
+# Set EPSG code
+EPSG_CODE="EPSG:4647"
+
+# Execute ogr2ogr command to extract power line data
+ogr2ogr -f "GeoJSON" -t_srs $EPSG_CODE -where "other_tags LIKE '%\"power\"=>\"line\"%'" "$output_file" "$input_file" lines
+
+echo "Power line data extraction complete."
