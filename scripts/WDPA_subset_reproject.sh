10efa46d5d72af0561086235af316e3bbaccb56c
Martin <hofer@iiasa.ac.at>
2024-12-10 16:47:47 +0100

    filter columns.

--- scripts/WDPA_subset_reproject.sh
+++ scripts/WDPA_subset_reproject.sh
@@ -28,17 +28,23 @@ xmax=33
 ymax=69
 
 # Step 1: Subset the shapefiles using the bounding box and filter by MARINE != 2
-ogr2ogr -f "ESRI Shapefile" $subset0 $shp0 -spat $xmin $ymin $xmax $ymax -where "MARINE <> '2'"
-ogr2ogr -f "ESRI Shapefile" $subset1 $shp1 -spat $xmin $ymin $xmax $ymax -where "MARINE <> '2'"
-ogr2ogr -f "ESRI Shapefile" $subset2 $shp2 -spat $xmin $ymin $xmax $ymax -where "MARINE <> '2'"
+ogr2ogr -f "ESRI Shapefile" $subset0 $shp0 -spat $xmin $ymin $xmax $ymax -where "MARINE <> '2'" -select MARINE,WDPA_PID,name
+ogr2ogr -f "ESRI Shapefile" $subset1 $shp1 -spat $xmin $ymin $xmax $ymax -where "MARINE <> '2'" -select MARINE,WDPA_PID,name
+ogr2ogr -f "ESRI Shapefile" $subset2 $shp2 -spat $xmin $ymin $xmax $ymax -where "MARINE <> '2'" -select MARINE,WDPA_PID,name
+
 
 # Step 2: Merge the three subsets into a single shapefile
 ogr2ogr -f "ESRI Shapefile" $merged $subset0
 ogr2ogr -f "ESRI Shapefile" -update -append $merged $subset1 -nln merged_shp
 ogr2ogr -f "ESRI Shapefile" -update -append $merged $subset2 -nln merged_shp
 
+
 # Step 3: Reproject the merged shapefile to EPSG:4647
 ogr2ogr -f "ESRI Shapefile" -t_srs "EPSG:4647" $output_vector $merged
 
+
 # Clean up intermediate files
 rm -f $subset0 $subset1 $subset2
+
+echo WDPA protected areas merged to a single EU shapefile at $merged 
+
